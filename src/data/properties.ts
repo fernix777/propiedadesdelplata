d14667fcbb5a3508dcca11f1c928737c1b6c939b
fernando bettiol <djfernix@gmail.com>
2025-01-13 20:33:51 -0300

    cuarta actualizacion

--- src/data/properties.ts
+++ src/data/properties.ts
@@ -10,7 +10,7 @@ export const properties: Property[] = [
     bedrooms: 4,
     bathrooms: 3,
     area: 280,
-    imageUrl: 'https://postimg.cc/SnpBf3M7',
+    imageUrl: '/public/assets/images/casa_residencial_gobernacion/logo_tarjeta.jpg',
     description: 'Hermosa casa moderna con amplios espacios, jardín y terraza.'
   },
   {
@@ -22,7 +22,7 @@ export const properties: Property[] = [
     bedrooms: 1,
     bathrooms: 1,
     area: 52,
-    imageUrl: 'https://postimg.cc/185HSKh1',
+    imageUrl: '/public/assets/images/local_comercial/logo_tarjeta.jpg',
     description: 'Alquiler de local comercial en zona comercial, apto para todo rubro.'
   },
   {
@@ -34,7 +34,7 @@ export const properties: Property[] = [
     bedrooms: 3,
     bathrooms: 3,
     area: 1250,
-    imageUrl: '/public/assets/images/casa_quinta/logo_tarjeta.jpg',
+    imageUrl: '/public/assets/images/logo_tarjeta.jpg',
     description: 'Casa quinta tres Dorm. Uno en Suite, Star Living, cocina,Comedor  tres Baños , Dependencias de servicio  pileta,garaje,Placares despensa Agua Calenté termotanque muro Perimetral portón Electrico Lote de 25 X 50  título impuestos al día SE ACEPTA LOTE HASTA 5 HECTAREAS ZONA HIPODROMO, ITAEMBE GUAZU'
   }
 ];
